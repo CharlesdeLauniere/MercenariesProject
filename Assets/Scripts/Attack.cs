365b3af5c180c05d39c59aee84ebfa875931ef96
Louis-Félix Laliberté <124931837+yanSoloo04@users.noreply.github.com>
2023-02-16 16:00:58 -0500

    2

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+
+    [SerializeField] private int _initiative;
+    [SerializeField] private int _vie;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
