d718275f9fb02214a348d24cf2414d7b74a449c6
CharlesdeLauniere <charles.de.launiere.01@edu.cegeptr.qc.ca>
2023-02-20 11:44:24 -0500

    unifinished player mouvement

--- Assets/Scripts/MouseCollider.cs
+++ Assets/Scripts/MouseCollider.cs
@@ -7,11 +7,14 @@ public class MouseCollider : MonoBehaviour
 {
     
     [SerializeField] private GameObject tile;
+    [SerializeField] private GameObject cubeplayer;
     void OnMouseDown()
     {
+        
          GameObject cube = GameObject.Find("CubePlayer");
         float zTile = tile.transform.position.z;
         float xTile = tile.transform.position.x;
         cube.transform.position = new Vector3(xTile, 0.2f, zTile);
+        cubeplayer.GetComponent<PlayerMouvement>().onMove();
     }
 }
--- Assets/Scripts/PlayerMouvement.cs
+++ Assets/Scripts/PlayerMouvement.cs
@@ -6,7 +6,60 @@ using UnityEngine;
 public class PlayerMouvement : MonoBehaviour
 {
     [SerializeField] private int _range;
+    [SerializeField] private GameObject cubeplayer;
+    [SerializeField] private GameObject acessibleTileIndicator;
+
+
+    public void onMove()
+    {
+        if (_range >0)
+        {
+            float z = cubeplayer.transform.position.z;
+            float x = cubeplayer.transform.position.x;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    GameObject cube = GameObject.Find($"Cube {x + i - 1} {z + j - 1}");
+
+                    cube.GetComponent<Collider>().enabled = !cube.GetComponent<Collider>().enabled;
+                    Instantiate(acessibleTileIndicator, new Vector3(x + i - 1, .125f, z + j - 1), Quaternion.identity);
+
+                }
+            }
+            _range--;
+
+        }
+
+
+    }
+
+    private void Start()
+    {
+        if (_range > 0)
+        {
+            float z = cubeplayer.transform.position.z;
+            float x = cubeplayer.transform.position.x;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    GameObject cube = GameObject.Find($"Cube {x + i - 1} {z + j - 1}");
+
+                    cube.GetComponent<Collider>().enabled = !cube.GetComponent<Collider>().enabled;
+                    Instantiate(acessibleTileIndicator, new Vector3(x + i - 1, .125f, z + j - 1), Quaternion.identity);
+
+                }
+            }
+            _range = -1;
+
+        }
+
+
+    }
+
+}
+
 
     // GameObject mapGenerator = GameObject.Find("MapGenerator");
     //GameObject[,] matrix = mapGenerator.GetComponent<MapGenerator>.getMapMatrix();
-}
