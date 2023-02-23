6d05319664cc51ce6c445bddabd1f2d5ba5ab3a2
yanSoloo04 <124931837+yanSoloo04@users.noreply.github.com>
2023-02-23 17:36:21 -0500

    Progress bar

---
+++ Assets/Scripts/Chevalier.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chevalier : MonoBehaviour
+{
+    [SerializeField] private int _initiative;
+    [SerializeField] private float _vie;
+    [SerializeField] private float _attaque;
+    [SerializeField] private float _attaque2;
+    [SerializeField] private float _attaque3;
+}
---
+++ Assets/Scripts/RoundSystem.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoundSystem : MonoBehaviour
+{
+
+    public Chevalier _chevalier;
+
+    public enum TurnState
+    {
+        processing, 
+        waiting,
+        selecting,
+        action,
+        dead,
+        next
+    }
+    public TurnState currentState;
+    private float cur_cooldown = 0f;
+    private float max_cooldown = 5f;
+    public Image Timer;
+    void Start()
+    {
+        currentState = TurnState.processing;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState)
+        {
+            case (TurnState.processing):
+                UpdateProgressBar();
+
+                break;
+
+            case (TurnState.action):
+
+
+                break;
+
+            case (TurnState.selecting):
+
+
+                break;
+
+            case (TurnState.waiting):
+
+
+                break;
+
+            case (TurnState.dead):
+
+
+                break;
+
+            case (TurnState.next):
+
+
+                break;
+        }
+    }
+
+    void UpdateProgressBar()
+    {
+        cur_cooldown = cur_cooldown + Time.deltaTime;
+        float calc_cooldown = cur_cooldown / max_cooldown;
+        Timer.transform.localScale = new Vector3(Mathf.Clamp(calc_cooldown, 0, 1), Timer.transform.localScale.y, Timer.transform.localScale.z);
+        if (cur_cooldown >= max_cooldown)
+        {
+            currentState = TurnState.next;
+        }
+    }
+    
+    void finTour()
+    {
+
+    }
+}
