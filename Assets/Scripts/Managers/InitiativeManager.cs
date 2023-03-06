92b05f1c915a97c88c2ae8b53e199fcf578db438
BrandonPageau <124932183+BrandonPageau@users.noreply.github.com>
2023-03-06 11:16:26 -0500

    Cartes

---
+++ Assets/Scripts/Managers/InitiativeManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitiativeManager : MonoBehaviour
+{
+    [SerializeField] GameObject _personnage1;
+    [SerializeField] GameObject _personnage2;
+    [SerializeField] GameObject _personnage3;
+    [SerializeField] GameObject _personnage4;
+    [SerializeField] GameObject _personnage5;
+    [SerializeField] GameObject _personnage6;
+    [SerializeField] GameObject _personnage7;
+
+    [SerializeField] int initiative1;
+    [SerializeField] int initiative2;
+    [SerializeField] int initiative3;
+    [SerializeField] int initiative4;
+    [SerializeField] int initiative5;
+    [SerializeField] int initiative6;
+    [SerializeField] int initiative7;
+
+
+    [SerializeField] int nbPersonnage;
+
+    void Start()
+    {
+      
+
+
+    }
+
+  
+}
