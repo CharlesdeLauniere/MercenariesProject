1e06ea8832572f5aa9164144f2465f95540e68ab
yanSoloo04 <124931837+yanSoloo04@users.noreply.github.com>
2023-05-01 11:08:35 -0400

    Mana bug fix je pense

--- Assets/Scripts/Utils/Enums.cs
+++ Assets/Scripts/Utils/Enums.cs
@@ -22,7 +22,8 @@ namespace MercenariesProject
         Multiply,
         Divide,
         AddByPercentage,
-        MinusByPercentage
+        MinusByPercentage,
+        AddMana
     }
 
     public enum TileTypes
